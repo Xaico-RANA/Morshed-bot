8cf0cff4c839cec382afc7f1f508abcc0142a2df
Mohammad RANA <ruhulaminrana98@gmail.com>
2025-02-05 21:25:52 +0600

    Update auto.js

--- scripts/commands/auto.js
+++ scripts/commands/auto.js
@@ -17,20 +17,20 @@ handleEvent: async function ({ api, event, args }) {
     const fs = require("fs-extra")
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  const {alldown} = require("nayan-videos-downloader")
+  const {alldown} = require("rana-video-downloader")
   if (body.startsWith("https://")) {
   api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
 const data = await alldown(content);
   console.log(data)
   const {low, high, title} = data.data;
-    api.setMessageReaction("✔️", event.messageID, (err) => {}, true);
+    api.setMessageReaction("✔✅", event.messageID, (err) => {}, true);
   const video = (await axios.get(high, {
       responseType: "arraybuffer",
     })).data;
     fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
 
         return api.sendMessage({
-            body: `《TITLE》: ${title}`,
+            body: `Dowloaded By Morshed\n《TITLE》: ${title}`,
             attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
 
         }, event.threadID, event.messageID);
